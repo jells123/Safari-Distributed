aad9c1e8d21c8b470af91b6ca5113b327366c7e5
jells123 <julka.bedziechowska@gmail.com>
2018-05-19 20:21:59 +0200

    Fixez

--- turysta.cpp
+++ turysta.cpp
@@ -38,6 +38,7 @@ int MAX_ORGS;
 
 pthread_mutex_t tab_mtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t inviteResponses_mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t myGroup_mtx = PTHREAD_MUTEX_INITIALIZER;
 
 vector<processInfo> tab; // T == size??
 vector<int> permissions, queue;
@@ -51,13 +52,12 @@ void *receiveMessages(void *ptr) {
         //println("czekam na wiadomości...\n");
         MPI_Recv( &pkt, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-        if (pkt.type != NOT_ORG)
-            println("wiadomość: %s od %d\n", msgTypes[pkt.type], status.MPI_SOURCE);
+        //if (pkt.type != NOT_ORG)
+        //    println("wiadomość: %s od %d\n", msgTypes[pkt.type], status.MPI_SOURCE);
         for (int i = 0; i < handlers.size(); i++) {
             if (handlers[i].msgType == pkt.type)
                 handlers[i].handler( &pkt, status.MPI_SOURCE ); 
         }
-
         /*
         packet *newpkt = (packet*) malloc(sizeof(packet));
         memcpy(newpkt, (const char *)&pkt, sizeof(packet));
@@ -67,6 +67,58 @@ void *receiveMessages(void *ptr) {
     }
 
     return (void *)0;
+}
+
+
+vector<int> myGroup;
+vector<int> invitations;
+void *orgThreadFunction(void *ptr) { // NOPE, MYLĄCA NAZWA FUKNCJI ;)
+
+    packet pkt;
+    //while ( true ) {
+
+        //if (currentRole == ORG) {
+            invitations.clear();
+
+            //pthread_mutex_lock(&myGroup_mtx);
+            //int groupSize = myGroup.size(); 
+
+            while (myGroup.size() != G-1) {
+
+                pthread_mutex_lock(&inviteResponses_mtx);
+                inviteResponses = 0;
+                pthread_mutex_unlock(&inviteResponses_mtx);
+
+                vector<int>::iterator it;
+                int choice, missing = G-1 - myGroup.size();
+
+                for ( int i = 0; i < missing; ++i ) {
+                    do {
+                        choice = rand()%T;
+                        it=find(invitations.begin(), invitations.end(), choice);
+                    } while (choice == tid || it != invitations.end());
+                    invitations.push_back(choice);
+                }
+
+                for (int i = 0; i < missing; ++i) {
+                    int idx = invitations.size() - 1 - i;
+                    packet msg = { timestamp, INVITE, missing };
+                    MPI_Send( &msg, 1, MPI_PAKIET_T, invitations[idx], MSG_TAG, MPI_COMM_WORLD);
+                    println("%d invited :) \n", invitations[idx]);
+                }
+
+                while (inviteResponses != missing) ;
+
+            }
+            println("I've got a group!\n");
+            //while (true) ;
+            
+        //}
+
+    //}
+
+    return (void *)0;
+
 }
 
 void randomRole() {
@@ -88,68 +140,30 @@ void randomRole() {
                     MPI_Send( &msg, 1, MPI_PAKIET_T, i, MSG_TAG, MPI_COMM_WORLD);
         }
     }
-}
 
-vector<int> myGroup;
-vector<int> invitations;
-void *sendMessages(void *ptr) { // NOPE, MYLĄCA NAZWA FUKNCJI ;)
-
-    packet pkt;
-    while ( true ) {
-
-        if (currentRole == ORG) {
-
-            while (myGroup.size() != G) {
-
-                pthread_mutex_lock(&inviteResponses_mtx);
-                inviteResponses = 0;
-                invitations.clear();
-                pthread_mutex_unlock(&inviteResponses_mtx);
-
-                vector<int>::iterator it;
-                int choice, missing = G - myGroup.size();
-
-                for ( int i = 0; i < G; ++i ) {
-                //while (invitations.size() != G) {
-                    do {
-                        choice = rand()%T;
-                        it=find(invitations.begin(), invitations.end(), choice);
-                    } while (choice == tid || it != invitations.end());
-                    invitations.push_back(choice);
-                }
-                //}
-
-                //for (auto i : invitations) {
-                for (int i = 0; i < G; ++i) {
-                    int idx = invitations.size() - 1 - i;
-                    packet msg = { timestamp, INVITE, missing };
-                    MPI_Send( &msg, 1, MPI_PAKIET_T, idx, MSG_TAG, MPI_COMM_WORLD);
-                }
-
-                while (inviteResponses != invitations.size()) ;
-
-            }
-
-            println("I've got a group!\n");
-            
-        }
-
-    }
-
-    return (void *)0;
+    if (currentRole == ORG)
+        pthread_create( &sender_th, NULL, orgThreadFunction, 0 );
 
 }
-
-   // ACCEPT, // przyjęcie zaproszenia 
-   //  REJECT_HASGROUP, // odmowa - mam już grupę
-   //  REJECT_ISORG, // odmowa - jestem organizatorem
 
 void inviteHandler(packet *pkt, int src) {
+    pthread_mutex_lock(&myGroup_mtx);
+
     if (currentRole == TUR && myGroup.empty()) {
         myGroup.push_back(src);
         packet msg = { timestamp, ACCEPT, 0 };
         MPI_Send( &msg, 1, MPI_PAKIET_T, src, MSG_TAG, MPI_COMM_WORLD);   
     }
+    else if (currentRole == TUR && !myGroup.empty()) {
+        packet msg = { timestamp, REJECT_HASGROUP, myGroup[0] };
+        MPI_Send( &msg, 1, MPI_PAKIET_T, src, MSG_TAG, MPI_COMM_WORLD);   
+    }
+    else if (currentRole == ORG) {
+        packet msg = { timestamp, REJECT_ISORG, G - myGroup.size() };
+        MPI_Send( &msg, 1, MPI_PAKIET_T, src, MSG_TAG, MPI_COMM_WORLD); 
+    }
+
+    pthread_mutex_unlock(&myGroup_mtx);
 }
 
 void not_orgHandler(packet *pkt, int src) {
@@ -161,20 +175,25 @@ void not_orgHandler(packet *pkt, int src) {
     }
     if (currentRole == TUR
         && T - touristsCount < MAX_ORGS 
-        && MAX_ORGS - (T - touristsCount) > tid ) { // o jeden za mało?
+        && MAX_ORGS - (T - touristsCount) >= tid ) { // o jeden za mało?
         currentRole = ORG;
         println("I became ORG! Because I could.\n");
+
+        if (currentRole == ORG)
+            pthread_create( &sender_th, NULL, orgThreadFunction, 0 );
     }
 }
 
 void acceptHandler(packet *pkt, int src) {
     pthread_mutex_lock(&inviteResponses_mtx);
     inviteResponses++;
+    myGroup.push_back(src);
     pthread_mutex_unlock(&inviteResponses_mtx);
 
-    myGroup.push_back(src);
     tab[src].role = TUR;
     tab[src].value = tid;
+
+    println("%d joining my group!", src);
 }
 
 void reject_hasgroupHandler(packet *pkt, int src) {
@@ -184,6 +203,8 @@ void reject_hasgroupHandler(packet *pkt, int src) {
 
     tab[src].role = TUR;
     tab[src].value = pkt->info_val;
+
+    println("%d already has group :/\n", src);
 
 }
 
@@ -195,6 +216,7 @@ void reject_isorgHandler(packet *pkt, int src) {
     tab[src].role = ORG;
     tab[src].value = pkt->info_val;
 
+    println("%d is org too. \n", src);
 }
 
 
@@ -235,7 +257,7 @@ int main(int argc, char * * argv) {
 
     prepare();
 
-    pthread_create( &sender_th, NULL, sendMessages, 0);
+    // pthread_create( &sender_th, NULL, orgThreadFunction, 0);
     pthread_create( &receiver_th, NULL, receiveMessages, 0);
 
     packet msg;
